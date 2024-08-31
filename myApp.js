d7eb6f97dc00386b9301711dbd46c5401f748c2f
Erwin Antepuesto <antepuesto.erwin@gmail.com>
2024-08-31 16:40:56 +0000

    add

--- myApp.js
+++ myApp.js
@@ -1,40 +1,43 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-console.log("Hello World")
+require("dotenv").config();
 
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
 
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+app.get("/json", (req, res) => {
+  res.json(
+    process.env.MESSAGE_STYLE === "uppercase"
+      ? { message: "HELLO JSON" }
+      : { message: "Hello json" }
+  );
+});
 
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({
+      time: req.time,
+    });
+  }
+);
 
+app.get("/:word/echo", (req, res) => {
+  res.json({
+    echo: req.params.word,
+  });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
